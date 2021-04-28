db9a4049624fb6be503177a7aa031aff286d6180
Luiz Alberto Alano <alano@luigipietro.com.br>
2021-04-27 23:05:02 -0300

    Aula Desenvolvimento básico em Java - Commit inicial

---
+++ exemplo-maven/src/test/java/br/com/luigipietro/exemplomaven/ExemploMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luigipietro.exemplomaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
